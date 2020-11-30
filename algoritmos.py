0dce78433d60e865575347ee88c129bc03e27ff5
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-11-29 18:56:00 -0600

    Comencé a agregar la pre-vizualisación del trabajo

---
+++ algoritmos.py
@@ -0,0 +1,8 @@
+"""
+Carlos Paredes Márquez.
+"""
+
+import math #Importar libreria de Matemáticas.
+
+def distancia_euclidiana(origen_x, origen_y, destino_x, destino_y): #Definir nuestra 'función'.
+    return math.sqrt((destino_x - origen_x)**2 + (destino_y - origen_y)**2) #Retornar nuestra 'función'.
