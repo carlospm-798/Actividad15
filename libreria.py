c58ec69fe37d376aaf199ce8ad4be4859ec17260
Carlos Paredes <carlos.paredes@alumnos.udg.mx>
2020-12-09 13:46:32 -0600

    Actualización de repositorio

--- libreria.py
+++ libreria.py
@@ -5,6 +5,7 @@ Libreria de particulas.
 from particula import Particula
 import json
 from pprint import pprint, pformat
+from collections import deque
 
 class Libreria:
     def __init__(self):
@@ -17,8 +18,8 @@ class Libreria:
         for particula in self.__particulas:
             print(particula)
     
-    def mostrar_diccionario(self):
-        diccionario = dict()
+    def mostrar_diccionario(self, grafo):
+        #diccionario = dict()
 
         for particula in self.__particulas:
             key = particula.origen_x, particula.origen_y
@@ -26,17 +27,68 @@ class Libreria:
             key_2 = particula.destino_x, particula.destino_y
             value_2 = particula.origen_x, particula.origen_y, particula.distancia
 
-            if key in diccionario:
-                diccionario[key].append(value)
+            if key in grafo:
+                grafo[key].append(value)
             else:
-                diccionario[key] = [value]
-            if key_2 in diccionario:
-                diccionario[key_2].append(value_2)
+                grafo[key] = [value]
+            if key_2 in grafo:
+                grafo[key_2].append(value_2)
             else:
-                diccionario[key_2] = [value_2]
+                grafo[key_2] = [value_2]
                 
-        str = pformat(diccionario, width = 40, indent = 1)
+        str = pformat(grafo, width = 40, indent = 1)
         return str
+    
+    def metodo_p(self, grafo, origen_x = 0, origen_y = 0):
+        self.mostrar_diccionario(grafo)
+        key = (origen_x, origen_y)
+
+        if key in grafo:
+            pila = []
+            visitados = []
+            recorrido = []
+
+            pila.append(key)
+            visitados.append(key)
+
+            while len(pila) > 0:
+                vertice = pila[-1]
+                recorrido.append(vertice)
+                pila.pop
+                adyacente = grafo[vertice] #key error 0
+                for i in adyacente:
+                    if not i[0] in visitados:
+                        visitados.append(i[0])
+                        pila.append(i[0])
+            return recorrido
+        else:
+            return 0
+    
+    def metodo_a(self, grafo, origen_x=0, origen_y=0):
+        self.mostrar_diccionario(grafo)
+        key = (origen_x, origen_y)
+
+        if key in grafo:
+            cola = deque()
+            visitados = []
+            recorrido = []
+
+            cola.appendleft(key)
+            visitados.append(key)
+
+            while len(cola) > 0:
+                vertice = cola[-1]
+                recorrido.append(vertice)
+                cola.pop()
+                
+                adyacente = grafo[vertice]
+                for i in adyacente:
+                    if not i in visitados:
+                        visitados.append(i[0])
+                        cola.appendleft(i[0])
+            return recorrido
+        else:
+            return 0
 
     def __str__(self):
         return "".join(
